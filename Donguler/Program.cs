8af4e2aa8892cb2e538ccde3b0ebc5d24455af0f
ufukk <ufukk@DESKTOP-T5LKVEB>
2021-10-09 22:33:27 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[]
+            {
+                "Yazılım geliştirici yetiştirme kampı","Programlamaya başlangıç için temel kurs", "Java", "Python"
+            };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            
+        }
+    }
+}
