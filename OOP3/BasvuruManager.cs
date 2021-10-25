75f0e71e704d6edc3f57def18dacb468e783fc0c
Ufuk Keleş <ufukkeles93@hotmail.com>
2021-10-25 21:26:48 +0300

    25.10.2021

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler, ILoggerService loggerService)
+        {
+            foreach (IKrediManager kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+            loggerService.Log();
+        }
+    }
+}
